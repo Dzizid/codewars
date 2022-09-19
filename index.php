4c048c438fa2ccfac64044373e8aeef12eacaecb
Александр <sanecehek56@gmail.com>
2022-09-19 22:21:18 +0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+//README берем строку и каждую цыфру в ней умножаем на неё же, после чего выводим без пробелов
+//Например, если мы пропустим через функцию 9119, получится 811181, потому что 9**2 равно 81, а 1**2 равно 1.
+/*
+function square_digits($num){
+
+    $numbers = str_split($num);
+
+    foreach($numbers as &$number) {
+        $number *= $number;
+    }
+
+    var_dump(implode($numbers));
+}
+
+square_digits(9119);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
